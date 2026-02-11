5f7dffe61e63e95c6c6a7fb7e2b2835c553554ed
Jumpei Tamura <gtamu.dip@gmail.com>
2026-02-11 19:49:50 +0900

    feat: add server mock test

--- playwright-handson/tests/form.spec.ts
+++ playwright-handson/tests/form.spec.ts
@@ -15,3 +15,16 @@ test('フォーム操作のテスト', async ({ page }) => {
   await page.getByRole('button', { name: /シャッフル/ }).click();
   await expect(page.getByRole('status', { name: /結果/})).toHaveText(/(太郎→花子)|(花子→太郎)/);
 });
+
+test('フォーム操作のテスト(サーバモック)', async ({ page }) => {
+  await page.route('/api/shuffle', async route => {
+    const response = [{members: ['太郎', '花子', '三郎']}];
+    await route.fulfill({ json: response });
+  });
+  await page.goto('http://localhost:3000/form');
+  await page.getByRole('textbox', { name: /一人目/ }).fill('太郎');
+  await page.getByRole('textbox', { name: /二人目/ }).fill('花子');
+  await page.getByRole('textbox', { name: /三人目/ }).fill('三郎');
+  await page.getByRole('button', { name: /シャッフル/ }).click();
+  await expect(page.getByRole('status', { name: /結果/})).toHaveText(/太郎→花子→三郎/);
+});
